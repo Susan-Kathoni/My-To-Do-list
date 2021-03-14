283dd6dd70f1785993021e9af8fac07c824b9871
Susan-Kathoni <123joey4kathoniey@gmail.com>
2021-03-14 17:25:16 +0300

    Add functions.

---
+++ script.js
@@ -0,0 +1,11 @@
+const listContainer = document.querySelector('[toDo-List]')
+
+let toDoList = []
+
+function render() {
+    clearElement(toDoListContainer)
+}
+
+function clearElement(element) {
+    
+}
